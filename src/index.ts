8db70572c626045698b439b0faa13de86bdb5bdc
Elsayed Kamal <sayed.kamal@waseet.net>
2025-03-09 01:16:19 +0200

    “wip”

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,11 @@
 import app from './app';
 
 // Only start the server in development
-if (process.env.NODE_ENV !== 'production') {
+// if (process.env.NODE_ENV !== 'production') {
   const PORT = process.env.PORT || 3000;
   app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
   });
-}
+// }
 
 export default app;
