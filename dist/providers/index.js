18c460a7edfc971a4fbff4be5f19f797ec4f890e
Elsayed Kamal <sayed.kamal@waseet.net>
2025-03-09 00:05:52 +0200

    “wip”

--- dist/providers/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.providers = void 0;
-// src/providers/index.ts
-const whvx_1 = require("./whvx");
-exports.providers = [whvx_1.WHVX]; // Add more providers as needed
